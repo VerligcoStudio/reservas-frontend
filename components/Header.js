51e35748a0cc4b847708f8538c37ea08c62cb1a1
VerligcoStudio <verlig.co@gmail.com>
2025-11-19 18:02:32 -0300

    Create Header.js

---
+++ components/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header style={{padding: '20px', background: '#000', color: 'white'}}>
+      <h1 style={{margin: 0}}>Sistema de Reservas</h1>
+    </header>
+  );
+}
