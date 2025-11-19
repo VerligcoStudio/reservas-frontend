474d2e240166e9a506f2d29227d7b4384b28305b
VerligcoStudio <verlig.co@gmail.com>
2025-11-19 18:06:37 -0300

    Create page.js

---
+++ app/reservas/page.js
@@ -0,0 +1,43 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Header from "../../components/Header";
+
+export default function Reservas() {
+  const [reservas, setReservas] = useState([]);
+
+  const carregar = async () => {
+    const res = await fetch("https://SEU-BACKEND.onrender.com/reservas");
+    const data = await res.json();
+    setReservas(data);
+  };
+
+  const remover = async (id) => {
+    await fetch(`https://SEU-BACKEND.onrender.com/reservas/${id}`, {
+      method: "DELETE"
+    });
+    carregar();
+  };
+
+  useEffect(() => {
+    carregar();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <h2>Reservas feitas</h2>
+
+        {reservas.map((r) => (
+          <div className="card" key={r.id}>
+            <strong>{r.nome}</strong>
+            <p>Pessoas: {r.pessoas}</p>
+            <p>Horário: {r.horario}</p>
+            <button onClick={() => remover(r.id)}>Excluir</button>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
